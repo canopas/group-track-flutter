199a08f05bb9d1416dca34c811b9833044b86401
kaushik <kaushik.s@canopas.con>
2024-07-01 16:10:33 +0530

    Remove config

--- data/lib/config.dart
+++
@@ -1,4 +0,0 @@
-class AppConfig {
-  static const String placeBaseUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
-  static const String mapApiKey = 'AIzaSyDbaJSVGU4Jkhd_V_e9esorzh_8yykh160';
-}
