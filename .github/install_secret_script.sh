59aaeae86efc28f5cece6f07ec39cb9237eaebbe
kaushik <kaushik.s@canopas.con>
2024-07-01 19:19:50 +0530

    Minor

--- .github/install_secret_script.sh
+++ .github/install_secret_script.sh
@@ -1,10 +1,10 @@
 env:
+  CONFIG_DART_BASE64: ${{ secrets.CONFIG_DART_BASE64 }}
   FIREBASE_OPTIONS_BASE64: ${{ secrets.FIREBASE_OPTIONS_BASE64 }}
   GOOGLE_SERVICES_JSON_BASE64: ${{ secrets.GOOGLE_SERVICES_JSON_BASE64 }}
-  CONFIG_DART_BASE64: ${{ secrets.CONFIG_DART_BASE64 }}
 
-echo $FIREBASE_OPTIONS_BASE64 | base64 -di > lib/firebase_options.dart
-echo $GOOGLE_SERVICES_JSON_BASE64 | base64 -di > android/app/google-services.json
 cd ../data
 echo $CONFIG_DART_BASE64 | base64 -di > lib/config.dart
 cd ../app
+echo $FIREBASE_OPTIONS_BASE64 | base64 -di > lib/firebase_options.dart
+echo $GOOGLE_SERVICES_JSON_BASE64 | base64 -di > android/app/google-services.json
