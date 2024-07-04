517c6f234db228e75da4cee785479e2cd54a29b9
kaushik <kaushik.s@canopas.con>
2024-07-04 15:10:54 +0530

    base to base64

--- .github/install_secret_script.sh
+++ .github/install_secret_script.sh
@@ -5,4 +5,4 @@ env:
 
 echo $FIREBASE_OPTIONS_BASE64 | base64 -di > lib/firebase_options.dart
 echo $GOOGLE_SERVICES_JSON_BASE64 | base64 -di > android/app/google-services.json
-echo $INFO_PLIST_BASE64 | base -di > ios/Runner/Info.plist
+echo $INFO_PLIST_BASE64 | base64 -di > ios/Runner/Info.plist
