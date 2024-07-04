0fa6b4a595c2327aad262c27d9c9807ba4a5cf1b
kaushik <kaushik.s@canopas.con>
2024-07-04 15:05:43 +0530

    info base64

--- .github/install_secret_script.sh
+++ .github/install_secret_script.sh
@@ -1,6 +1,8 @@
 env:
   FIREBASE_OPTIONS_BASE64: ${{ secrets.FIREBASE_OPTIONS_BASE64 }}
   GOOGLE_SERVICES_JSON_BASE64: ${{ secrets.GOOGLE_SERVICES_JSON_BASE64 }}
+  INFO_PLIST_BASE64: ${{ secrets.INFO_PLIST_BASE64 }}
 
 echo $FIREBASE_OPTIONS_BASE64 | base64 -di > lib/firebase_options.dart
 echo $GOOGLE_SERVICES_JSON_BASE64 | base64 -di > android/app/google-services.json
+echo $INFO_PLIST_BASE64 | base -di > ios/Runner/Info.plist
