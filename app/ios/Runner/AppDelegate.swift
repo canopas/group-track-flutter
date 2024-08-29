049f37ea5f91da8024942d909eebd3509ed55f91
cp-ishita-g <ishita.g@canopas.com>
2024-08-29 15:52:02 +0530

    add battery monitor in app delegate

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -4,13 +4,14 @@ import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      let key = Bundle.main.object(forInfoDictionaryKey: "ApiMapKey")
-      GMSServices.provideAPIKey(key as! String)
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        let key = Bundle.main.object(forInfoDictionaryKey: "ApiMapKey")
+        GMSServices.provideAPIKey(key as! String)
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
