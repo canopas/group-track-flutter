2c955752b96790b62dd11c60a23fbe10e7758e7e
kaushik <kaushik.s@canopas.con>
2024-09-10 12:05:24 +0530

    Update user state when received state notification

--- data/lib/service/network_service.dart
+++ data/lib/service/network_service.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../api/auth/auth_models.dart';
 import '../log/logger.dart';
@@ -13,18 +15,37 @@ class NetworkService {
           fromFirestore: ApiUser.fromFireStore,
           toFirestore: (user, options) => user.toJson());
 
-  Future<void> _updateUserState(String id, int state) async {
-    await _userRef.doc(id).update({
-      "state": state,
-      "updated_at": DateTime.now().millisecondsSinceEpoch,
-    });
-  }
-
   void updateUserNetworkState(String id) async {
     if (id.isNotEmpty) {
-      _updateUserState(id, USER_STATE_ONLINE);
+      final userState = await checkUserState();
+
+      await _userRef.doc(id).update({
+        "state": userState,
+        "updated_at": DateTime.now().millisecondsSinceEpoch,
+      });
     } else {
       logger.e("NetworkService: Error while update user network state");
     }
+  }
+
+  Future<int> checkUserState() async {
+    var result = await Connectivity().checkConnectivity();
+    final isConnected = result.first == ConnectivityResult.mobile ||
+        result.first == ConnectivityResult.wifi;
+    final isLocationEnabled = await isLocationAlwaysEnabled();
+
+    if (isConnected && isLocationEnabled) {
+      return USER_STATE_ONLINE;
+    } else if (!isLocationEnabled) {
+      return USER_STATE_LOCATION_PERMISSION_DENIED;
+    } else {
+      return USER_STATE_NO_NETWORK_OR_PHONE_OFF;
+    }
+  }
+
+  Future<bool> isLocationAlwaysEnabled() async {
+    final isLocationEnable = await Permission.location.isGranted;
+    final isBackgroundEnable = await Permission.locationAlways.isGranted;
+    return (isLocationEnable && isBackgroundEnable);
   }
 }
