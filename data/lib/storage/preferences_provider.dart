aee192513f8af8ce7433c6fbacb9421b8258c69c
cp-ishita-g <ishita.g@canopas.com>
2024-10-22 11:43:42 +0530

    fix lint

--- data/lib/storage/preferences_provider.dart
+++ data/lib/storage/preferences_provider.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final sharedPreferencesProvider = Provider<SharedPreferences>((ref) => throw UnimplementedError());
+final sharedPreferencesProvider =
+    Provider<SharedPreferences>((ref) => throw UnimplementedError());
 
 StateProvider<T> createPrefProvider<T>({
   required String prefKey,
   required T defaultValue,
 }) {
-  return StateProvider<T>((ref) {
+  return StateProvider((ref) {
     final prefs = ref.watch(sharedPreferencesProvider);
 
     final currentValue = prefs.get(prefKey) as T? ?? defaultValue;
