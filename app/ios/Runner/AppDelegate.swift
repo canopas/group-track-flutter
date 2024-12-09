2c8852f6f7b26610f0c359ba5a42701372d42c9b
Radhika Canopas <radhika.s@canopas.com>
2024-12-09 15:29:18 +0530

    WIP - Improve CLLocationManager integration

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,68 +1,147 @@
-import UIKit
+import CoreLocation
 import FirebaseMessaging
 import Flutter
 import GoogleMaps
-import flutter_background_service_ios
 import CoreLocation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
 
     var geofencePlugin: GeofenceService?
-    var locationManager: CLLocationManager?
     var lastUpdateTime: Date?
 
     override func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]?
     ) -> Bool {
         UIDevice.current.isBatteryMonitoringEnabled = true
-        
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-        }
 
-        setUpLocation()
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate =
+                self as UNUserNotificationCenterDelegate
+        }
 
         let key = Bundle.main.object(forInfoDictionaryKey: "ApiMapKey")
         GMSServices.provideAPIKey(key as! String)
-        
+
         getLocationMethodRegistration()
         geofencePluginRegistration()
+        registerLocationChannel()
+
+        let locationsHandler = LocationsHandler.shared
+        if locationsHandler.updatesStarted {
+            locationsHandler.startLocationUpdates()
+        }
+
+        var cancellable = locationsHandler.$lastLocation.sink(receiveValue: { [weak self] location in
+           
+            let locationData: [String: Any] = [
+                "latitude": location.coordinate.latitude,
+                "longitude": location.coordinate.longitude,
+                "timestamp": location.timestamp.timeIntervalSince1970 * 1000,
+            ]
+
+            guard let self = self else { return }
+            if let controller = window?.rootViewController
+                as? FlutterViewController
+            {
+                let methodChannel = FlutterMethodChannel(
+                    name: "com.grouptrack/location",
+                    binaryMessenger: controller.binaryMessenger)
+                methodChannel.invokeMethod(
+                    "onLocationUpdate", arguments: locationData)
+            }
+        })
 
         GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        return super.application(
+            application, didFinishLaunchingWithOptions: launchOptions)
     }
-    
-    func application(application: UIApplication,
-                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+    func application(
+        application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
         Messaging.messaging().apnsToken = deviceToken
+    }
+
+    func getLocationMethodRegistration() {
+        let controller: FlutterViewController =
+            window?.rootViewController as! FlutterViewController
+        let locationChannel = FlutterMethodChannel(
+            name: "com.grouptrack/current_location",
+            binaryMessenger: controller.binaryMessenger)
+
+        locationChannel.setMethodCallHandler {
+            [weak self]
+            (call: FlutterMethodCall, result: @escaping FlutterResult) in
+    
+            if call.method == "getCurrentLocation" {
+                LocationsHandler.shared.getCurrentLocation(result: result)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
     }
 }
 
 // Geofence methods
 extension AppDelegate {
+    private func registerLocationChannel() {
+        let controller: FlutterViewController =
+            window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(
+            name: "com.grouptrack/location", binaryMessenger: controller.binaryMessenger
+        )
+        
+        channel.setMethodCallHandler {
+            [weak self]
+            (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            guard let self = self else { return }
+            if call.method == "startTracking" {
+                LocationsHandler.shared.startLocationUpdates()
+                result(true)
+            } else if call.method == "stopTracking" {
+                LocationsHandler.shared.stopLocationUpdates()
+                result(true)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
+    }
+    
+    
     private func geofencePluginRegistration() {
-        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        let geofenceChannel = FlutterMethodChannel(name: "geofence_plugin", binaryMessenger: controller.binaryMessenger)
+        let controller: FlutterViewController =
+            window?.rootViewController as! FlutterViewController
+        let geofenceChannel = FlutterMethodChannel(
+            name: "geofence_plugin", binaryMessenger: controller.binaryMessenger
+        )
 
         geofencePlugin = GeofenceService(channel: geofenceChannel)
 
-        geofenceChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+        geofenceChannel.setMethodCallHandler {
+            [weak self]
+            (call: FlutterMethodCall, result: @escaping FlutterResult) in
             guard let self = self else { return }
             if call.method == "startMonitoring" {
                 if let args = call.arguments as? [String: Any],
-                   let lat  = args["latitude"] as? Double,
-                   let lng = args["longitude"] as? Double,
-                   let radius = args["radius"] as? Double,
-                   let id = args["identifier"] as? String {
-                    let center = CLLocationCoordinate2D(latitude: lat, longitude: lng)
-                    self.geofencePlugin?.startMonitoring(center: center, radius: radius, identifier: id)
+                    let lat = args["latitude"] as? Double,
+                    let lng = args["longitude"] as? Double,
+                    let radius = args["radius"] as? Double,
+                    let id = args["identifier"] as? String
+                {
+                    let center = CLLocationCoordinate2D(
+                        latitude: lat, longitude: lng)
+                    self.geofencePlugin?.startMonitoring(
+                        center: center, radius: radius, identifier: id)
                     result(nil)
                 }
             } else if call.method == "stopMonitoring" {
                 if let args = call.arguments as? [String: Any],
-                   let id = args["identifier"] as? String {
+                    let id = args["identifier"] as? String
+                {
                     self.geofencePlugin?.stopMonitoring(identifier: id)
                     result(nil)
                 }
@@ -71,81 +150,5 @@ extension AppDelegate {
             }
         }
     }
-}
 
-// Location delegate methods
-extension AppDelegate: CLLocationManagerDelegate {
-    private func setUpLocation() {
-        locationManager = CLLocationManager()
-        locationManager?.delegate = self
-        locationManager?.distanceFilter = 10
-        locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager?.allowsBackgroundLocationUpdates = true
-        locationManager?.pausesLocationUpdatesAutomatically = false
-        locationManager?.startUpdatingLocation()
-    }
-    
-    func getCurrentLocation(result: @escaping FlutterResult) {
-        guard let locationManager = self.locationManager,
-              let location = locationManager.location else {
-            result(FlutterError(code: "LOCATION_ERROR", message: "Location data not available", details: nil))
-            return
-        }
-        
-        let locationData: [String: Any] = [
-            "latitude": location.coordinate.latitude,
-            "longitude": location.coordinate.longitude,
-            "timestamp": location.timestamp.timeIntervalSince1970 * 1000
-        ]
-        result(locationData)
-    }
-    
-    func getLocationMethodRegistration() {
-        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        let locationChannel = FlutterMethodChannel(name: "com.grouptrack/current_location", binaryMessenger: controller.binaryMessenger)
-        
-        locationChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
-            guard let self = self else { return }
-            
-            if call.method == "getCurrentLocation" {
-                self.getCurrentLocation(result: result)
-            } else {
-                result(FlutterMethodNotImplemented)
-            }
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let currentLocation = locations.last else { return }
-        
-        let currentTime = Date()
-        
-        if let lastTime = lastUpdateTime {
-            let timeInterval = currentTime.timeIntervalSince(lastTime)
-            if timeInterval < 10 {
-                return
-            }
-        }
-        
-        lastUpdateTime = currentTime
-        
-        let locationData: [String: Any] = [
-            "latitude": currentLocation.coordinate.latitude,
-            "longitude": currentLocation.coordinate.longitude,
-            "timestamp": currentLocation.timestamp.timeIntervalSince1970 * 1000
-        ]
-        
-        if let controller = window?.rootViewController as? FlutterViewController {
-            let methodChannel = FlutterMethodChannel(name: "com.grouptrack/location", binaryMessenger: controller.binaryMessenger)
-            methodChannel.invokeMethod("onLocationUpdate", arguments: locationData)
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedAlways || status == .authorizedWhenInUse {
-            locationManager?.startMonitoringSignificantLocationChanges()
-        } else {
-            locationManager?.stopUpdatingLocation()
-        }
-    }
 }
---
+++ app/ios/Runner/LocationManager.swift
@@ -0,0 +1,96 @@
+//
+//  LocationManager.swift
+//  Runner
+//
+//  Created by Radhika S on 04/12/24.
+//
+
+import CoreLocation
+
+@MainActor
+class LocationsHandler: NSObject, ObservableObject, CLLocationManagerDelegate {
+    static let shared = LocationsHandler()
+    private let manager: CLLocationManager
+
+    @Published var lastLocation = CLLocation()
+
+    @Published
+    var updatesStarted: Bool = UserDefaults.standard.bool(
+        forKey: "liveLocationUpdatesStarted")
+    {
+        didSet {
+            UserDefaults.standard.set(updatesStarted, forKey: "liveLocationUpdatesStarted")
+        }
+    }
+
+    private override init() {
+        self.manager = CLLocationManager()
+        super.init()
+        manager.delegate = self
+        manager.distanceFilter = 10
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.allowsBackgroundLocationUpdates = true
+        manager.pausesLocationUpdatesAutomatically = false
+    }
+    
+    func startLocationUpdates() {
+        if self.manager.authorizationStatus == .notDetermined {
+           print("startLocationUpdates: permission notDetermined")
+            return
+       }
+        updatesStarted = true
+         print("startLocationUpdates: startUpdatingLocation......")
+        manager.startUpdatingLocation()
+    }
+
+    func stopLocationUpdates() {
+        manager.stopUpdatingLocation()
+        updatesStarted = false
+    }
+    
+    func locationManagerDidChangeAuthorization(
+        _ manager: CLLocationManager,
+        didChangeAuthorization status: CLAuthorizationStatus
+    ) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            updatesStarted = true
+            manager.startUpdatingLocation()
+        } else {
+            updatesStarted = false
+            manager.stopUpdatingLocation()
+        }
+    }
+
+    func locationManagerDidUpdateLocations(
+        _ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]
+    ) {
+        guard let currentLocation = locations.last else { return }
+
+    
+        let lastUpdateTime = lastLocation.timestamp
+
+        let timeInterval = Date().timeIntervalSince(lastUpdateTime)
+        if timeInterval < 10 {
+            return
+        }
+        
+        print("Got location update: \(currentLocation.coordinate.latitude):\(currentLocation.coordinate.longitude)")
+
+        lastLocation = currentLocation
+    }
+    
+    func getCurrentLocation(result: @escaping FlutterResult) {
+        guard let location =  self.manager.location else {
+            result(FlutterError(code: "LOCATION_ERROR", message: "Location data not available", details: nil))
+            return
+        }
+        
+        let locationData: [String: Any] = [
+            "latitude": location.coordinate.latitude,
+            "longitude": location.coordinate.longitude,
+            "timestamp": location.timestamp.timeIntervalSince1970 * 1000
+        ]
+        result(locationData)
+    }
+    
+}
